4520c2b1e77df7db2ba8e5e4e2720978204b2c65
ramusama09 <53930520+ramusama09@users.noreply.github.com>
2025-05-13 11:11:03 +0800

    correct print size, error 2 pages

--- obj/Debug/net8.0/PDAO_WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PDAO_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PDAO_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f018d61103beed7e251a668524e78ff63285921")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0031996f5d208e79130799a2da411e8765cbc617")]
 [assembly: System.Reflection.AssemblyProductAttribute("PDAO_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PDAO_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
