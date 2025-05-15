4042039874804eb77c093dc183e51aa8edfb68b4
ramusama09 <53930520+ramusama09@users.noreply.github.com>
2025-05-15 08:49:47 +0800

    Fixed Printing 1 page, Needs Centering and Hide Preview

--- obj/Debug/net8.0/PDAO_WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PDAO_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PDAO_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0031996f5d208e79130799a2da411e8765cbc617")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4520c2b1e77df7db2ba8e5e4e2720978204b2c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("PDAO_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PDAO_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
